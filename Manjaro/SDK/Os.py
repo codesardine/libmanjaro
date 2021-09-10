8c47134c151f1ac6cd805847b0be07fe1ffb8345
Vitor Lopes <vmnlop@gmail.com>
2021-09-10 15:03:39 +0100

    update osinfo

---
+++ Manjaro/SDK/Os.py
@@ -0,0 +1,24 @@
+import os, subprocess
+
+
+class Info():
+
+    def __init__(self):
+        self.inv_variables = self.get_inv_variables()
+
+    def get_inv_variables(self) -> dict: 
+        output = subprocess.check_output("printenv", shell=True)
+        dict = {}
+        s = output.decode('utf-8').splitlines()
+        for i in s:
+            split = i.split("=", 1)
+            dict[split[0]] = str(split[1])          
+        return dict
+
+        
+    def get_lsb_version(self):
+        lsb = Popen(["lsb_release"], shell=shell)
+        return lsb.replace("LSB Version:", "").trim()
+
+    
+    
