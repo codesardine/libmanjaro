076aedbd5798dc4a2588ba2455b1008f4b2f017a
Vitor Lopes <vmnlop@gmail.com>
2021-09-12 16:35:37 +0100

    update install

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='manjaro-sdk',
     version='0.1',
-    packages=['Manjaro/SDK'],
+    packages=['Manjaro'],
     package_dir={"": "src"},
     url='https://github.com/Manjaro-WebDad/libmanjaro.git',
     license='GPL',
